63aeaef140794a257b997e58633b2bd7275cbe4f
Aylin DeBruyne <adebruyne88@gmail.com>
2019-01-19 10:26:21 -0500

    update and show edited article working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,11 @@ def new
     @article = Article.new
 end
 
-def create
+def edit
+    @article = Article.find(params[:id])
+end
 
+def create
     @article = Article.new (article_params)
     if @article.save
         #do something
@@ -13,13 +16,22 @@ def create
     else
         render 'new'
     end
+end
 
-
+def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+        flash[:notice] = "Article was successfully updated"
+        redirect_to article_path(@article)
+    else 
+        render 'edit'
+    end
 end
 
 def show
     @article = Article.find(params[:id])
 end
+
 
 
 
