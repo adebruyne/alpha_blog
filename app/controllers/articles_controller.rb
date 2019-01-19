62e65a6ee2ce3de44143a973db158f6f002fbc5b
Aylin DeBruyne <adebruyne88@gmail.com>
2019-01-19 10:09:44 -0500

    handling showing errors invalid input

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,13 +9,20 @@ def create
     if @article.save
         #do something
         flash[:notice] = "Article was successfully created"
-        redirect_to artcile_path(@article)
+        redirect_to article_path(@article)
     else
         render 'new'
     end
 
 
 end
+
+def show
+end
+
+
+
+
 
 private
     def article_params      
