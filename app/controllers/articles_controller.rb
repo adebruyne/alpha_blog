17cc8af501c7940872f4608ac6349cf4f0dae6d3
Aylin DeBruyne <adebruyne88@gmail.com>
2019-01-21 10:30:17 -0500

    adds styles to flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,7 @@ def create
     @article = Article.new (article_params)
     if @article.save
         #do something
-        flash[:notice] = "Article was successfully created"
+        flash[:success] = "Article was successfully created"
         redirect_to article_path(@article)
     else
         render 'new'
@@ -29,7 +29,7 @@ end
 def update
     
     if @article.update(article_params)
-        flash[:notice] = "Article was successfully updated"
+        flash[:success] = "Article was successfully updated"
         redirect_to article_path(@article)
     else 
         render 'edit'
@@ -43,7 +43,7 @@ end
 def destroy
     
     @article.destroy
-    flash[:notice] = "Article was succesfully deleted"
+    flash[:danger] = "Article was succesfully deleted"
     redirect_to articles_path
 end
 
