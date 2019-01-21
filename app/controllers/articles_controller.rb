111fd21a88fc1a5216f1720c525127fcd9cc82f0
Aylin DeBruyne <adebruyne88@gmail.com>
2019-01-20 19:56:38 -0500

    adds a home route and about route and destroy methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -38,7 +38,12 @@ def show
     @article = Article.find(params[:id])
 end
 
-
+def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was succesfully deleted"
+    redirect_to articles_path
+end
 
 
 
